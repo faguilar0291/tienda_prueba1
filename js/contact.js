d9658d9952a569a2f9042f3b19ed47c1ed97542e
Fernando Aguilar <fernandoaguilar@MacBook-Air-de-Fernando.local>
2023-12-30 13:11:17 -0300

    Avances 15

--- js/contact.js
+++ js/contact.js
@@ -1,15 +1,72 @@
-let form = document.querySelector("form");
-let input = document.querySelector("input");
+function validateForm(event) {
+    // Reset errors
+    resetErrors();
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let valor = input.value;
-    let longitud = valor.length;
+    // Get form values
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var email = document.getElementById("email").value;
+    var cellPhone = document.getElementById("cellPhone").value;
+    var message = document.getElementById("message").value;
 
-    console.log(valor);
-    console.log(longitud);
+    // Validate each field
+    var isValid = true;
 
-    if (longitud < 3){
-        console.log("menor a 3");
+    if (!isValidName(firstName)) {
+        document.getElementById("firstNameError").innerText = "Invalid first name";
+        isValid = false;
     }
-});
+
+    if (!isValidName(lastName)) {
+        document.getElementById("lastNameError").innerText = "Invalid last name";
+        isValid = false;
+    }
+
+    if (!isValidEmail(email)) {
+        document.getElementById("emailError").innerText = "Invalid email address";
+        isValid = false;
+    }
+
+    if (!isValidPhone(cellPhone)) {
+        document.getElementById("cellPhoneError").innerText = "Invalid cell phone number";
+        isValid = false;
+    }
+
+    if (!isValidMessage(message)) {
+        document.getElementById("messageError").innerText = "Message must be at least 250 characters";
+        isValid = false;
+    }
+
+    // If any field is not valid, prevent form submission
+    if (!isValid) {
+        event.preventDefault();
+    }
+}
+
+function resetErrors() {
+    // Reset error messages
+    var errorElements = document.getElementsByClassName("error");
+    for (var i = 0; i < errorElements.length; i++) {
+        errorElements[i].innerText = "";
+    }
+}
+
+function isValidName(name) {
+    // Simple validation for names (allow only alphabets and spaces)
+    return /^[a-zA-Z\s]+$/.test(name);
+}
+
+function isValidEmail(email) {
+    // Simple validation for email address
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+function isValidPhone(phone) {
+    // Simple validation for phone number (allow only digits and optional hyphens)
+    return /^\d+(-\d+)*$/.test(phone);
+}
+
+function isValidMessage(message) {
+    // Check if the message has at least 250 characters
+    return message.length <= 250;
+}
